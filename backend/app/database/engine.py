be7805d398325eb44092519c8df378c3608cbdbb
SharifWaqas <shaarif.1031@gmail.com>
2026-06-29 01:27:55 +0500

    refactor(database): migrate SQLAlchemy to async infrastructure

--- backend/app/database/engine.py
+++ backend/app/database/engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 from backend.app import config
 
 db_host = config.DB_HOST
@@ -6,7 +6,7 @@ db_name = config.DB_NAME
 db_password = config.DB_PASSWORD
 db_port = str(config.DB_PORT)
 db_user = config.DB_USER
-db_url = "postgresql://" + db_user + ":" +db_password +"@" + db_host +":"+db_port +"/"+db_name
+db_url = "postgresql+asyncpg://" + db_user + ":" +db_password +"@" + db_host +":"+db_port +"/"+db_name
 
 
-engine = create_engine(db_url)
+engine = create_async_engine(db_url)
--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,5 +1,5 @@
 from backend.app.database import engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 db_engine = engine.engine
-Sessionfactory = sessionmaker(bind=db_engine)
+SessionFactory = async_sessionmaker(bind=db_engine)
