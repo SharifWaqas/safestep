1b849972bc9e97af86b03a2d145dc50990cee860
SharifWaqas <shaarif.1031@gmail.com>
2026-07-02 02:25:50 +0500

    feat(repository): add find_by_email method

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from backend.app.models.user import User
 from backend.app.repositories.base_repository import BaseRepository
@@ -7,4 +8,12 @@ from backend.app.repositories.base_repository import BaseRepository
 class UserRepository(BaseRepository[User]):
 
     def __init__(self, db_session: AsyncSession):
-        super().__init__(db_session=db_session, model=User)        
+        super().__init__(db_session=db_session, model=User)
+
+    async def find_by_email(self, email: str) -> User | None:
+        query = select(self._model).where(self._model.email == email)
+        result = await self._db_session.execute(query)
+        return result.scalar_one_or_none()
+
+
+        
