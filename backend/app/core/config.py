c369766f3356f79451e604c76eb54a8e2563bfa9
SharifWaqas <shaarif.1031@gmail.com>
2026-07-05 15:59:05 +0500

    feat(core): add application configuration

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    model_config = SettingsConfigDict(env_file=".env",env_file_encoding="utf-8",)
+    JWT_SECRET : str
+    JWT_ALGORITHM : str
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
+    REFRESH_TOKEN_EXPIRE_DAYS : int
+
+settings = Settings()
