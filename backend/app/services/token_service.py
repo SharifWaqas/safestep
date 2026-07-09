657604755346cc93f8c24c03ef879ad4c55e3569
SharifWaqas <shaarif.1031@gmail.com>
2026-07-10 02:54:41 +0500

    feat(services): implement session creation

--- backend/app/services/token_service.py
+++ backend/app/services/token_service.py
@@ -1,13 +1,34 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import hashlib
 
+from datetime import UTC, datetime
+
+from backend.app.models.session import Session
 from backend.app.repositories.session_repository import SessionRepository
 from backend.app.services.jwt_service import JWTService
+from backend.app.models.user import User
 
 
 class TokenService:
 
-    def __init__(self,db: AsyncSession, session_repository: SessionRepository, jwt_service: JWTService) -> None:
-        self._db = db
+    def __init__(self, session_repository: SessionRepository, jwt_service: JWTService) -> None:
         self._session_repository = session_repository
         self._jwt_service = jwt_service
     
+    def _hash_refresh_token(self, refresh_token: str) -> str:
+        return hashlib.sha256(refresh_token.encode("utf-8")).hexdigest()
+
+
+    async def create_session(self, user: User, refresh_token: str) -> None:
+        payload = self._jwt_service.verify_token(refresh_token)
+
+        expires_at = datetime.fromtimestamp(payload["exp"], UTC)        
+        
+        refresh_token_hash = self._hash_refresh_token(refresh_token)
+        
+        session = Session(
+            user_id=user.id,
+            refresh_token_hash=refresh_token_hash,
+            expires_at=expires_at
+            )
+            
+        await self._session_repository.save(session)
