b3d9a6b4464960880ff3e227f73505f76b4e16a1
SharifWaqas <shaarif.1031@gmail.com>
2026-07-23 03:05:42 +0500

    Improved readibility of APi

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -9,11 +9,13 @@ from backend.app.schemas.auth import LoginRequest, LoginResponse, RegisterRespon
 
 router = APIRouter(prefix="/auth", tags=["Authentication",])
 
+
+"Login"
 @router.post("/login",response_model=LoginResponse)
 async def login(request: LoginRequest, auth_service: AuthService = Depends(get_auth_service)):
     return await auth_service.login(email=request.email,password=request.password)
 
-
+"Register"
 @router.post("/register", response_model=RegisterResponse)
 async def register(request: RegisterRequest, auth_service: AuthService = Depends(get_auth_service)) -> RegisterResponse:
     return await auth_service.register(request)
