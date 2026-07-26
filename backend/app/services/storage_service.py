b7c959fcb6b8c901ee9f352cfc1a755701a433c0
SharifWaqas <shaarif.1031@gmail.com>
2026-07-27 00:03:46 +0500

    Implement asynchronous file storage service

--- backend/app/services/storage_service.py
+++ backend/app/services/storage_service.py
@@ -1,5 +1,7 @@
 from fastapi import UploadFile
 from pathlib import Path
+import uuid
+import aiofiles
 
 from backend.app.schemas.upload import StorageResult
 
@@ -10,7 +12,16 @@ class StorageService:
         self._upload_directory.mkdir(parents=True, exist_ok=True)
 
     async def save_file(self, file: UploadFile)-> StorageResult:
-        ...
+        file_path = Path(file.filename)
+        file_extension = file_path.suffix
+        file_id = uuid.uuid4()
+        stored_file_name = f"{file_id}{file_extension}"
+        storage_path = self._upload_directory / stored_file_name
+        file_data = await file.read()
+        async with aiofiles.open(storage_path, "wb") as f:
+            await f.write(file_data)
+
+        return StorageResult(storage_path=str(storage_path),file_name=stored_file_name,file_size= len(file_data),content_type= file.content_type)
 
     def delete_file(self):
         ...
