af4ba8913f945800a60300674c3f5a5bade83fe7
SharifWaqas <shaarif.1031@gmail.com>
2026-09-20 21:47:44 -0500

    Update sitemap for custom domain

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://safestep-rust.vercel.app/sitemap.xml",
+    sitemap: "https://www.usesafestep.com/sitemap.xml",
   };
 }
